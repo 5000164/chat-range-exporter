d619350efca84e4f31d73b2784da8c2551706654
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-04-17 08:48:19 +0900

    refactor: Slack に関する処理をクラスに抜き出した

--- src/main/scala/jp/_5000164/chat_range_exporter/interfaces/Application.scala
+++ src/main/scala/jp/_5000164/chat_range_exporter/interfaces/Application.scala
@@ -1,18 +1,7 @@
 package jp._5000164.chat_range_exporter.interfaces
 
-import akka.actor.ActorSystem
-import slack.api.BlockingSlackApiClient
-import slack.rtm.SlackRtmClient
-
-import scala.concurrent.ExecutionContextExecutor
-
 object Application extends App {
-  implicit val system: ActorSystem = ActorSystem("slack")
-  implicit val ec: ExecutionContextExecutor = system.dispatcher
-  val token = sys.env("SLACK_TOKEN")
-  val rtmClient = SlackRtmClient(token)
-  val apiClient = BlockingSlackApiClient(token)
-  val botId = rtmClient.state.self.id
-  val operator = new Operator(rtmClient, botId, apiClient)
-  rtmClient.onMessage(message => operator.run(message))
+  val slack = new Slack(sys.env("SLACK_TOKEN"))
+  val operator = new Operator(slack)
+  slack.start(operator)
 }
