5cfac1fc93483f03a70bbb9d7cc2efd2f341e57e
菅原 浩 <sncts35326@gmail.com>
2018-04-17 09:10:31 +0900

    feat: 取得したメッセージの結果を Backlog に書き出す

--- src/main/scala/jp/_5000164/chat_range_exporter/interfaces/Operator.scala
+++ src/main/scala/jp/_5000164/chat_range_exporter/interfaces/Operator.scala
@@ -4,12 +4,14 @@ import jp._5000164.chat_range_exporter.domain.Export
 import slack.SlackUtil
 import slack.models.Message
 
-class Operator(val slack: Slack) {
+class Operator(val slack: Slack, val backlog: Backlog) {
   def run(message: Message): Unit = {
     val mentionedIds = SlackUtil.extractMentionedIds(message.text)
     if (mentionedIds.contains(slack.botId)) {
       Export.execute(slack.apiClient, message.channel, message.text) match {
-        case Right(result) => slack.rtmClient.sendMessage(message.channel, result)
+        case Right(result) =>
+          backlog.write(result)
+          slack.rtmClient.sendMessage(message.channel, "成功")
         case Left(error) => slack.rtmClient.sendMessage(message.channel, error)
       }
     }
