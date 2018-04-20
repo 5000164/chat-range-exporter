e6a2861828da79a8eeb101d70a48731c7b71c1c8
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-04-21 05:04:09 +0900

    feat: スレッドのメッセージを取得する

--- src/main/scala/jp/_5000164/chat_range_exporter/domain/Export.scala
+++ src/main/scala/jp/_5000164/chat_range_exporter/domain/Export.scala
@@ -6,20 +6,28 @@ import play.api.libs.json.JsValue
 object Export {
   def execute(slack: Slack, channelId: String, message: String): Either[String, String] = {
     analyze(message) match {
-      case Right((oldest, latest)) =>
+      case Right((oldest, latest, true)) =>
         val messages = slack.fetchMessages(channelId, latest, oldest)
-        Right(transform(messages))
+        val replies = slack.fetchReplies(channelId, (messages.head \ "thread_ts").as[String])
+        Right(transform(replies))
+      case Right((oldest, latest, false)) =>
+        val messages = slack.fetchMessages(channelId, latest, oldest)
+        Right(transform(messages.reverse))
       case Left(error) =>
         Left(error)
     }
   }
 
-  def analyze(message: String): Either[String, (String, String)] = {
+  def analyze(message: String): Either[String, (String, String, Boolean)] = {
     message.split(' ') match {
+      case partList if partList.length == 2 =>
+        val oldest = transformTimestamp(getTimestamp(partList(1).drop(1).dropRight(1)), oldestFlag = true)
+        val latest = transformTimestamp(getTimestamp(partList(1).drop(1).dropRight(1)), oldestFlag = false)
+        Right((oldest, latest, true))
       case partList if partList.length == 3 =>
         val oldest = transformTimestamp(getTimestamp(partList(1).drop(1).dropRight(1)), oldestFlag = true)
         val latest = transformTimestamp(getTimestamp(partList(2).drop(1).dropRight(1)), oldestFlag = false)
-        Right((oldest, latest))
+        Right((oldest, latest, false))
       case _ => Left("引数の数がおかしいです")
     }
   }
@@ -34,7 +42,7 @@ object Export {
   }
 
   def transform(messages: Seq[JsValue]): String =
-    messages.map(filter).reverse.mkString("\n")
+    messages.map(filter).mkString("\n")
 
   private def filter(message: JsValue): String =
     s"${(message \ "ts").as[String]} ${(message \ "user").as[String]} ${(message \ "text").as[String]}"
--- src/test/scala/jp/_5000164/chat_range_exporter/domain/ExportSpec.scala
+++ src/test/scala/jp/_5000164/chat_range_exporter/domain/ExportSpec.scala
@@ -4,9 +4,14 @@ import org.scalatest.FreeSpec
 import play.api.libs.json.Json
 
 class ExportSpec extends FreeSpec {
-  "メッセージからリンクを取り出す" in {
+  "チャットメッセージの引数が 1 つの場合にタイムスタンプの範囲を取り出せる" in {
+    val message = "<@AAAAAAAAA> <https://my.slack.com/archives/AAAAAAAAA/p0000000000000001>"
+    assert(Export.analyze(message) == Right("0000000000.000000", "0000000000.000002", true))
+  }
+
+  "チャットメッセージの引数が 2 つの場合にタイムスタンプの範囲を取り出せる" in {
     val message = "<@AAAAAAAAA> <https://my.slack.com/archives/AAAAAAAAA/p0000000000000001> <https://my.slack.com/archives/AAAAAAAAA/p0000000000000002>"
-    assert(Export.analyze(message) == Right("0000000000.000000", "0000000000.000003"))
+    assert(Export.analyze(message) == Right("0000000000.000000", "0000000000.000003", false))
   }
 
   "Slack のメッセージをエクスポート用に整形する" in {
